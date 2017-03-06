655d7cc5cc147752f51f1836c9cf4f4e2e2acf76
Anne Charnes <timetwister4@yahoo.com>
2017-03-05 19:47:36 -0500

    Added email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
   has_one :dm_profile
   has_one :player_profile
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: {case_sensitive: false}
-  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+                    format: {with: VALID_EMAIL_REGEX }
   validates :password_digest, presence: true
 
   def to_json
