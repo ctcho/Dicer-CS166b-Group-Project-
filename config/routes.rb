1040a66f518e458c82c712aac3117ffe08e754e5
Anne Charnes <timetwister4@yahoo.com>
2017-03-06 13:50:04 -0500

    added log in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
-  root 'home_pages#home'
   get 'home_pages/home'
-
   get 'home_pages/about'
-
   get 'home_pages/contact'
-
   get 'home_pages/resources'
 
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users do
     resource :player_profiles do
       resources :character_sheets
     end
     resource :dm_profiles
   end
+
+  root 'home_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
