312bd871d469b6cc24705cbfcb2d1b6c2f59e014
Anne Charnes <timetwister4@yahoo.com>
2017-03-06 14:08:05 -0500

    fixed syntax error, removed byebugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,6 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      byebug
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
