5ae5d35fddca40818e051c15cfbd243b1dc719fc
vagrant <vagrant@precise64.(none)>
2017-03-08 06:02:52 +0000

    Incorporated all current changes to search pages

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -3,5 +3,6 @@ class SearchPagesController < ApplicationController
   end
 
   def results
+    @players = PlayerProfile.where("exp_level = ? AND ruleset = ?", params[:exp_level], params[:ruleset])
   end
 end
