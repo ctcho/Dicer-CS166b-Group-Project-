9fd99149797c3b6b48d4b62e4f8a786f70b0e394
Anne Charnes <timetwister4@yahoo.com>
2017-04-12 20:26:19 -0400

    reverted database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ User.create!(username:  "Example User",
              email: "example@dicer.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             address: "New York City",
+             address: "02453",
+             max_distance: 30,
              admin: true)
 
 #seeds an additional 25 regular users
@@ -25,10 +26,8 @@ User.create!(username:  "Example User",
                password: password,
                password_confirmation: password,
                age: age,
-               address: Faker::Address.state,
-               max_distance: 50
-               #address: "415 South Street, Waltham, MA 02454")
-               )
+               address: "415 South Street, Waltham, MA 02454",
+               max_distance: 30)
   u.player_profile = PlayerProfile.create(experience_level: 3, bio: "I have a bio",   online_play: 1,
     homebrew: 1,
     original_ruleset: 0,
