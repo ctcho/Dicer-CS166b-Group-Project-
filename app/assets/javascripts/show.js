f875b289036ec6ff0fb17433e51d39a7ae0079f5
Daniel <dvilinsk@brandeis.edu>
2017-05-09 10:56:34 -0400

    Commit msg

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,8 +1,8 @@
-$(".contact").click(function() {
-  $(".modall").css("display", "block");
-  $("textarea").val("Send new message..");
-  console.log("lorem ipsum dolor sit amet");
-});
+$("#contact").click(function() {
+    $(".modall").css("display", "block");
+    console.log("lorem ipsum dolor sit amet");
+  });
+}
 
 $(".close").click(function() {
   $("#msgmodal").css("display", "none");
