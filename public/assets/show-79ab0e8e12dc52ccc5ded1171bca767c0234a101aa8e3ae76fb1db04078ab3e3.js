bdd700996078b26cf806adc11d0c15531256e25f
Anne Charnes <timetwister4@yahoo.com>
2017-05-09 11:31:03 -0400

    fresh precompile of assets to fix multiple message actioncable bug

---
+++ public/assets/show-79ab0e8e12dc52ccc5ded1171bca767c0234a101aa8e3ae76fb1db04078ab3e3.js
@@ -0,0 +1,8 @@
+$("#contact").click(function() {
+    $(".modall").css("display", "block");
+    console.log("lorem ipsum dolor sit amet");
+});
+
+$(".close").click(function() {
+  $("#msgmodal").css("display", "none");
+});
