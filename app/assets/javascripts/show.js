ee029f537562ee53bd5795d23a9d68b864852e9c
Daniel <dvilinsk@brandeis.edu>
2017-05-09 11:04:48 -0400

    More JS fixes

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,8 +1,7 @@
 $("#contact").click(function() {
     $(".modall").css("display", "block");
     console.log("lorem ipsum dolor sit amet");
-  });
-}
+});
 
 $(".close").click(function() {
   $("#msgmodal").css("display", "none");
